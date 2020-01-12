f02b1d8a16b71b6def5d3e19e7e726b5869ceea5
Delor Rotstrump <delor4@gmail.com>
2020-01-12 20:33:56 +0100

    updated persons.index template, showing notice

--- resources/views/persons/index.blade.php
+++ resources/views/persons/index.blade.php
@@ -3,7 +3,15 @@
 @section('main')
 <div class="row">
 <div class="col-sm-12">
-    <h1 class="display-3">Persons</h1>    
+
+  @if(session()->get('success'))
+    <div class="alert alert-success">
+      {{ session()->get('success') }}  
+    </div>
+  @endif
+	</div>
+    
+<h1 class="display-3">Persons</h1>    
   <table class="table table-striped">
     <thead>
         <tr>

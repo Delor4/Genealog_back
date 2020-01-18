409653dcadd39bcf6d474ef88f2d710c08fe6d81
Delor Rotstrump <delor4@gmail.com>
2020-01-18 17:30:54 +0100

    format code, removing comments

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,11 +1,9 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
-//use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Http\Request;
@@ -14,16 +12,16 @@ use Illuminate\Auth\Events\Registered;
 class RegisterController extends Controller
 {
     /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
+     * |--------------------------------------------------------------------------
+     * | Register Controller
+     * |--------------------------------------------------------------------------
+     * |
+     * | This controller handles the registration of new users as well as their
+     * | validation and creation. By default this controller uses a trait to
+     * | provide this functionality without requiring any additional code.
+     * |
+     */
+    
     use RegistersUsers;
 
     /**
@@ -31,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/well';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -46,27 +44,42 @@ class RegisterController extends Controller
     /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
-        ]);
+        return Validator::make($data, []);
     }
+
     protected function validator_orig(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'name' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+                'unique:users'
+            ],
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'confirmed'
+            ]
         ]);
     }
-    
+
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param array $data
      * @return \App\User
      */
     protected function create(array $data)
@@ -74,44 +87,34 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => Hash::make($data['password'])
         ]);
     }
+
     protected function registered(Request $request, $user)
     {
-        //dd('Test1');
-        //dd(request()->all());
-        
         $user->generateToken();
         
-        //dd('Test');
-        
-        return response()->json(['data' => $user->toArray()], 201);
+        return response()->json([
+            'data' => $user->toArray()
+        ], 201);
     }
+
     /**
      * Handle a registration request for the application.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    
     public function register(Request $request)
     {
-        //return parent::register($request);
-        //dd('Test1');
-        //dd(request()->all());
+        $this->validator($request->all())
+            ->validate();
         
-        
-        $this->validator($request->all())->validate();
-        //return view('well');
-        //dd('Test2');
         event(new Registered($user = $this->create($request->all())));
-        //dd('Test3');
+        
         $this->guard()->login($user);
-        //dd('Test4');
-        return $this->registered($request, $user)
-        ?: redirect($this->redirectPath());
+        
+        return $this->registered($request, $user) ?: redirect($this->redirectPath());
     }
-    
-    
 }

b5e34ff0e5471ae7fa0933520714aa6b5e074338
Delor Rotstrump <delor4@gmail.com>
2020-01-18 17:08:02 +0100

    bug - use wrong class

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Request;
+
 
 class LoginController extends Controller
 {
